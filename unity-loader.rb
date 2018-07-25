6406b8e30b48f6907069d2c7165776cf10046bc0
Chris Cunningham <chris.cunningham@gogiigames.com>
2018-07-25 11:53:39 -0300

    unity-loader.rb added

--- unity-loader.rb
+++ unity-loader.rb
@@ -1,18 +1,18 @@
 class UnityLoader < Formula
-  desc 'Tools for running and installing multiple versions of Unity'
-  homepage 'https://github.com/cmcpasserby/unity-loader'
-  url 'https://github.com/cmcpasserby/unity-loader/archive/0.0.2.tar.gz'
-  sha256 'ce1f74e0710fbade92c9450a723a6381ab26dfe7993910910487ace72bab15bb'
+  desc "Tools for running and installing multiple versions of Unity"
+  homepage "https://github.com/cmcpasserby/unity-loader"
+  url "https://github.com/cmcpasserby/unity-loader/archive/0.0.3.tar.gz"
+  sha256 "40709f2399bea364368a9f36728c2e1a130182c5588eeff2db39860d467d8bc9"
 
-  depends_on 'go' => :build
-  depends_on 'dep' => :build
+  depends_on "go" => :build
+  depends_on "dep" => :build
 
   def install
-    ENV['GOPATH'] = buildpath
+    ENV["GOPATH"] = buildpath
     bin_path = buildpath/"src/github.com/cmcpasserby/unity-loader"
     bin_path.install Dir["*"]
     cd bin_path do
-      system "dep", "ensure"
+      system "dep", "ensure", "--vendor-only"
       system "go", "build", "-o", bin/"unity-loader", "cmd/unity-loader/main.go"
     end
   end
