1409bfb51dde7bcc8e88c85488a99e09d8ca4371
Chris Cunningham <chris.cunningham@gogiigames.com>
2018-09-11 14:00:48 -0300

    update checksum

--- unity-loader.rb
+++ unity-loader.rb
@@ -2,7 +2,7 @@ class UnityLoader < Formula
   desc "Tools for running and installing multiple versions of Unity"
   homepage "https://github.com/cmcpasserby/unity-loader"
   url "https://github.com/cmcpasserby/unity-loader/archive/v0.0.7.tar.gz"
-  sha256 "3c4f7a5516414753cd4b00104541bfef26b6fa314d8cb3b49b95c00b9405e2817"
+  sha256 "3c4f7a5516414753cd4b00104541bfef26b6fa314d8cb3b49b95c00b9405e281"
 
   depends_on "go" => :build
   depends_on "dep" => :build
