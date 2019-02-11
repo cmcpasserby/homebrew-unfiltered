e183f2ef0b672f1197e705cbc785d44b9c5636b2
Chris Cunningham <chris.cunningham@gogiigames.com>
2019-02-11 09:56:41 -0400

    added the v2.0.0 version of unity-loader

--- unity-loader.rb
+++ unity-loader.rb
@@ -1,8 +1,8 @@
 class UnityLoader < Formula
   desc "Tools for running and installing multiple versions of Unity"
   homepage "https://github.com/cmcpasserby/unity-loader"
-  url "https://github.com/cmcpasserby/unity-loader/archive/v0.0.7.tar.gz"
-  sha256 "3c4f7a5516414753cd4b00104541bfef26b6fa314d8cb3b49b95c00b9405e281"
+  url "https://github.com/cmcpasserby/unity-loader/archive/v2.0.0.tar.gz"
+  sha256 "ef4f6f9813f7cda2d983969083a035a69e6f71ca204238d532257dace41205ff"
 
   depends_on "go" => :build
   depends_on "dep" => :build
@@ -13,7 +13,7 @@ class UnityLoader < Formula
     bin_path.install Dir["*"]
     cd bin_path do
       system "dep", "ensure", "--vendor-only"
-      system "go", "build", "-o", bin/"unity-loader", "./cmd/unity-loader/"
+      system "go", "build", "-o", bin/"unity-loader", "./cmd/unity-loader/main.go"
     end
   end
 
