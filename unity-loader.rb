757b8187a65dc92b385c2b7ba944b4bd0083dfaf
Chris Cunningham <chris.cunningham@gogiigames.com>
2019-03-22 09:36:21 -0300

    changed formulas for go modules

--- unity-loader.rb
+++ unity-loader.rb
@@ -5,15 +5,12 @@ class UnityLoader < Formula
   sha256 "a0628ec0b27e8fa287a42261c8b41dd70089b109bf4823b05598046afc02edbb"
 
   depends_on "go" => :build
-  depends_on "dep" => :build
 
   def install
-    ENV["GOPATH"] = buildpath
-    bin_path = buildpath/"src/github.com/cmcpasserby/unity-loader"
+    bin_path = buildpath/"unity-loader"
     bin_path.install Dir["*"]
     cd bin_path do
-      system "dep", "ensure", "--vendor-only"
-      system "go", "build", "-o", bin/"unity-loader", "./cmd/unity-loader/main.go"
+      system "go", "build", "-mod=vendor" "-o", bin/"unity-loader", "./cmd/unity-loader/main.go"
     end
   end
 
