e426db5b7ebeec4678debd362d75cfee1992990e
Chris Cunningham <chris@rightsomegoodgames.ca>
2023-02-07 09:59:22 -0400

    updated unity-loader to 1.1.0

--- unity-loader.rb
+++ unity-loader.rb
@@ -1,8 +1,8 @@
 class UnityLoader < Formula
   desc "Tool for loading unity projects with their respective unity versions"
   homepage "https://github.com/cmcpasserby/unity-loader"
-  url "https://github.com/cmcpasserby/unity-loader/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "1db7c294a931a7f5282b0bd015d4d99690ffed0af4c8286ea5d37a92aee7280d"
+  url "https://github.com/cmcpasserby/unity-loader/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "cd9eefceeb2b2c55cdc72ef03296c3b73f21474c9ca06d19a4b5a7090e2a1294"
 
   depends_on "go" => :build
 
@@ -10,12 +10,11 @@ class UnityLoader < Formula
     bin_path = buildpath/"unity-loader"
     bin_path.install Dir["*"]
     cd bin_path do
-      system "go", "mod", "vendor"
-      system "go", "build", "-mod=vendor", "-o", bin/"unity-loader", "./cmd/unity-loader"
+      system "go", "build", "-o", bin/"unity-loader", "./cmd/unity-loader"
     end
   end
 
   test do
-    system "unity-loader", "--version"
+    system "unity-loader", "-v"
   end
 end
